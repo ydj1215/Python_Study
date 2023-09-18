f8a6431bd639dbb0f758d2c9754f819b7331b958
ydj1215 <ydjdj2@naver.com>
2023-09-18 17:48:07 +0900

    20230913

--- 코드업 기초 100제/6097번.py
+++ 코드업 기초 100제/6097번.py
@@ -1,19 +1,31 @@
 # 6097번
-def func(a,b,c,d,ls):
-	if b == 0: # 가로
-		for i in range(0,c):
-			ls[c-1][i] = 1
-	elif b == 1: # 세로
-		for i in range(0, c):
-			ls[i][d-1] = 1
+def func(a, b, c, d, ls):
+	if b == 0:  # 가로
+		for i in range(a):
+			if ls[c - 1][i] == 0:
+				ls[c - 1][i] = 1
+			else:
+				continue
+	elif b == 1:  # 세로
+		for i in range(a-1):
+			if ls[i+1][d-1] == 0:
+				ls[i+1][d - 1] = 1
+			else:
+				continue
+	return ls
+
 
 def printing(ls):
 	for e in range(len(ls)):
 		for i in range(len(ls[0])):
-			print(ls[e][i],end=' ')
+			print(ls[e][i], end=' ')
 		print()
 
-h,w=map(int,input().split())
-ls = [[0]*w for _ in range(h)]
-#n = int(input())
-# for i in range(n):
+
+h, w = map(int, input().split())
+ls = [[0] * w for _ in range(h)]
+x = int(input())
+for e in range(x):
+	a, b, c, d = map(int, input().split())
+	func(a, b, c, d, ls)
+printing(ls)
