1af6dd992c4a97267470c13eb5c4f7da27844c68
ydj1215 <ydjdj2@naver.com>
2023-09-19 16:53:29 +0900

    20230919

--- 코드업 기초 100제/6098번.py
+++ 코드업 기초 100제/6098번.py
@@ -1,14 +1,24 @@
-def find():
-	pass
+def find(ls):
+	# 테두리
+	for i in range(10):
+		ls[0][i] = 1
+		ls[i][0] = 1
+		ls[9][i] = 1
+		ls[i][9] = 1
+	# 출발
+	ls[1][1] = 9
+	for m in range(9):
+		for n in range(9):
+			if ls[m][n+1] != 1:
 
-ls = [[0]*10 for _ in range(10)]
-for e in ls:
-	print(e)
-	print()
-for i in range(10):
-	for j in range(10):
-		ls[i][j] = map(int,input().split())
 
-for e in ls:
-	print(e)
-	print()
+def printing(ls):
+	for i in range(len(ls)):
+		for j in range(len(ls[0])):
+			print(ls[i][j], end=' ')
+		print()
+
+
+ls = [[0] * 10 for _ in range(10)]
+find(ls)
+printing(ls)
